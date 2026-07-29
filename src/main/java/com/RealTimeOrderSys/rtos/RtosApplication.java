8e6985fc563b28e3a46dd4440b56791470c98c52
jyuu0107 <jyuu0107@student.monash.edu>
2026-07-29 23:44:48 +0800

    init

---
+++ src/main/java/com/RealTimeOrderSys/rtos/RtosApplication.java
@@ -0,0 +1,13 @@
+package com.RealTimeOrderSys.rtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RealTimeOrderSys/rtos/RtosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RealTimeOrderSys.rtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
